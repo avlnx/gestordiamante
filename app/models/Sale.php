20a4c42394872224cec0d522c4b1374623bfdded
Thyago Ferreira da Silva <thferreira@gmail.com>
2014-06-09 10:27:11 -0300

    Superadmin Sales desc order

--- app/models/Sale.php
+++ app/models/Sale.php
@@ -26,21 +26,23 @@ class Sale extends Eloquent
 		// Security: check if all ids in list sent via javascript are tenants controlled by this superadmin
 		// If we are here we are superadmins
 		$saemail = Auth::user()->email;
-		
+
 		// get all tenants_ids controlled by this superadmin
 		Sale::$clear_tenants = Tenant::where('email','=',$saemail)->lists('id');
-		
+
 		// Build an array from sent String tenants ids (10,23,43,4)
 		$sent_tenants_list = explode(',', $tenants_ids);
-		
+
 		// filter sent_tenants_list by tenants actually controlled by this superadmin
 		$filtered_tenants_list = array_filter($sent_tenants_list,function ($tenant){
 			return in_array($tenant, Sale::$clear_tenants);
 		});
-		
+
 		// get all sales whose tenant_id is in list of tenants above
-		$sales = Sale::whereIn('tenant_id', $filtered_tenants_list)->get();
-		
+		$sales = Sale::whereIn('tenant_id', $filtered_tenants_list)
+						->orderBy('created_at','desc')
+						->get();
+
       return $sales;
 	}
 
